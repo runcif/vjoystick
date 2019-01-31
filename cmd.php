9bdf934ff96276aef2ad97f7b2cbc5b21403207d
ir0nl3x <32999037+runcif@users.noreply.github.com>
2019-01-31 17:54:42 +0100

    Add files via upload

---
+++ cmd.php
@@ -0,0 +1,11 @@
+<?php
+echo "".$_GET["cmd"];
+
+//if($_GET['cmd'] == 1)
+//{
+
+//system("cd /usr/lib/cgi-bin && sudo python /home/pi/provaserva.py"); 
+//}
+//header("location: /joystick.html");
+
+?>
